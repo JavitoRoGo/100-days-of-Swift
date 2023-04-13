c66b81914ee3c052a56f57a596e462513da0010e
JavitoRoGo <javierrodriguezgomez79@gmail.com>
2023-04-13 18:33:52 +0100

    Project12a & Project12b added

---
+++ Project12a/Project12a/PersonalCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonalCell.swift
+//  Project10
+//
+//  Created by Javier Rodríguez Gómez on 31/5/21.
+//
+
+import UIKit
+
+class PersonalCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
---
+++ Project12b/Project12b/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Javier Rodríguez Gómez on 1/6/21.
+//
+
+import UIKit
+
+class Person: NSObject, Codable {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+
+}
