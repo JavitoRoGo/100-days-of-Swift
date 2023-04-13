dcd90fbe55935e9cfb27a21e661fb53521d838a4
JavitoRoGo <javierrodriguezgomez79@gmail.com>
2023-04-13 18:49:05 +0100

    Project18 added

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Javier Rodríguez Gómez on 7/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
